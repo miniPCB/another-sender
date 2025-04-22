20e860e1ffdc6ee9f51cf88af4b7aedff996ee11
miniPCB <nolan@minipcb.com>
2025-04-21 21:30:07 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="another-sender",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),  # <-- must include another_sender
     include_package_data=True,
     install_requires=[],
